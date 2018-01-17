04dec49903f8699d95148ca63be4c7df8a1f4233
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-17 10:07:45 -0700

    defined persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,5 +13,14 @@
 		<p><strong>Audience</strong>: This website will be designed for potential clients and professional peers.</p>
 		<p><strong>Goal</strong>: The primary goal of this website will be to attract new clients and acquire projects. Users of the site will be able to understand the business services we offer and initiate contact with us regarding projects or collaboration opportunities.</p>
 
+		<!-- Define a Persona representing the target audience for the PWP -->
+		<h3>Persona</h3>
+		<p><strong>Name</strong>: Floyd Platero<br>
+			<strong>Age</strong>: 43</p>
+		<p><strong>Profession</strong>: Tribal Administrator for a Tribal Nation. The Tribal Nation recently acquired grant funding for new and existing tribal programs, and they are looking for ways to improve these programs through the use of technology.</p>
+		<p><strong>Technology</strong>: Floyd uses a Windows computer with a Chrome Browser only while at work, and while he can navigate the internet and websites fairly well, he has limited technical knowledge.</p>
+		<p><strong>Attitudes and Behaviors</strong>: In his browsing of the internet, Floyd has come across useful websites for possible tribal programs, but he is cautious about content and information sharing. He tends to be reserved and private. and is interested in working with someone who .</p>
+		<p><strong>Needs and Goals</strong>: Floyd needs to find a website developer who can design and create websites for his Tribal Nation's tribal programs. He has an idea about the desired look and feel based on other websites he's come across, but he's not certain he'll be able to properly convey the specifications in a technical manner. Therefore, Floyd wants to be able work with someone he can relate to, someone he can communicate easily with, and someone who understands tribal goings-on. Floyd also wants the work done professionally and with privacy and security concerns in mind. Ideally, Floyd would like to work with a business that will be sensitive to Native cultural identity and that can offer other ideas for improving tribal programs.</p>
+		<p><strong>User Story</strong>: As a user, I need to determine whether this business can provide professional, secure, culturally relevant websites and technological solutions for my Tribal Nation.</p>
 	</body>
 </html>
