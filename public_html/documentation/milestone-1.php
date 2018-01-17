9d56a0fa12e87fc32356681c99fd0a79004d1610
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-17 09:32:23 -0700

    defined purpose, audience, goal

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>PWP: Milestone 1</title>
+	</head>
+	<body>
+		<h2>PWP: Milestone 1</h2>
+
+		<!-- Define Purpose, Audience, and Goal for the PWP -->
+		<h3>Purpose, Audience &amp; Goal</h3>
+		<p><strong>Purpose</strong>: This project will be small business website with a focus on front-end development.</p>
+		<p><strong>Audience</strong>: This website will be designed for potential clients and professional peers.</p>
+		<p><strong>Goal</strong>: The primary goal of this website will be to attract new clients and acquire projects. Users of the site will be able to understand the business services we offer and initiate contact with us regarding projects or collaboration opportunities.</p>
+
+	</body>
+</html>
