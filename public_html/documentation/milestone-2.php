d41813498e37360f26276163f63d8c7e1e47c24b
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-28 20:13:06 -0700

    added framework for content and wireframes sections

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,5 +9,23 @@
 
 		<!-- Document content to be featured in each section of the PWP site -->
 		<h2>Content Strategy</h2>
+		<p>My PWP site will be one page, and will have the following sections:</p>
+		<ul>
+			<li><strong>Welcome:</strong> ...<br></li>
+			<li><strong>About Us:</strong> ...<br></li>
+			<li><strong>Services:</strong> ...<br></li>
+			<li><strong>Contact:</strong> ...<br></li>
+		</ul>
+
+		<!-- Create wireframes using https://balsamiq.com -->
+		<h2>Wireframes</h2>
+		<p>This site will feature one layout that will be fully responsive on small and large screens.</p>
+		<h4>Full Screen Layout</h4>
+
+		<p>(insert image here)</p>
+
+		<h4>Mobile Layout</h4>
+
+		<p>(insert image here)</p>
 
 	</body>
