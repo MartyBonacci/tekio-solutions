bdc7824028ff2b16f6674c4e8fbdf51651f7f6d4
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-28 19:51:35 -0700

    created milestone 2 page with title

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>PWP: Milestone 2</title>
+	</head>
+	<body>
+		<h1>PWP: Milestone 2</h1>
+
+		<!-- Document content to be featured in each section of the PWP site -->
+		<h2>Content Strategy</h2>
+
+	</body>
