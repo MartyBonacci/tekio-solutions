fac60e88807be03510f61b9a125e94edf6157a34
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-29 21:47:20 -0700

    content strategy pretty much complete

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,16 +5,17 @@
 		<title>PWP: Milestone 2</title>
 	</head>
 	<body>
-		<h1>PWP: Milestone 2</h1>
+		<h1>PWP: Milestone 2&alpha;</h1>
 
 		<!-- Document content to be featured in each section of the PWP site -->
 		<h2>Content Strategy</h2>
 		<p>My PWP site will be one page, and will have the following sections:</p>
 		<ul>
-			<li><strong>Welcome:</strong> A brief introduction that clearly shows the business name and tagline. This welcome will be overlaid on a large background image (to be determined).<br></li>
-			<li><p><strong>About:</strong> This section will provide details about the business, including a brief description about the meaning of the business’s name. This section will also include details about me, the lead web developer, and list my skills along with links to professional social media profiles.</p></li>
+			<li><strong>Welcome:</strong> A brief introduction that clearly shows the business name and tagline. This welcome will be overlaid on a large background image (to be determined). There will be navigation at the top.<br></li>
+			<li><p><strong>About:</strong> This section will provide details about the business, including a brief description about the meaning of the business’s name. This section will also include details about me, the lead web developer, and list my skills along with links to professional social media profiles: LinkedIn, GitHub.</p></li>
 			<li><p><strong>Services:</strong> This section will highlight the Website Development and Digital Content Creation services offered by the business, including a brief description and iconography of each.</p></li>
 			<li><p><strong>Contact:</strong> This section will have a simple contact form, business mailing address, business email address, and phone number.</p></li>
+			<li><p><strong>Footer:</strong> Copyright information. (Future: social media links.)</p></li>
 		</ul>
 
 		<!-- Create wireframes using https://balsamiq.com -->
