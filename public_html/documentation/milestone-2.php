6ad00218f2bae5e259204e598dce671b3ecc417f
Jolynn Pruitt <jo@tekiosolutions.com>
2018-01-30 08:12:29 -0700

    added fullscreen wireframe image

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,7 +12,7 @@
 		<p>My PWP site will be one page, and will have the following sections:</p>
 		<ul>
 			<li><strong>Welcome:</strong> A brief introduction that clearly shows the business name and tagline. This welcome will be overlaid on a large background image (to be determined). There will be navigation at the top.<br></li>
-			<li><p><strong>About:</strong> This section will provide details about the business, including a brief description about the meaning of the business’s name. This section will also include details about me, the lead web developer, and list my skills along with links to professional social media profiles: LinkedIn, GitHub.</p></li>
+			<li><p><strong>About:</strong> This section will provide details about the business, including a brief description about the meaning of the business’s name. This section will also include details about me, and list my skills along with a small image and links to professional social media profiles: LinkedIn, GitHub.</p></li>
 			<li><p><strong>Services:</strong> This section will highlight the Website Development and Digital Content Creation services offered by the business, including a brief description and iconography of each.</p></li>
 			<li><p><strong>Contact:</strong> This section will have a simple contact form, business mailing address, business email address, and phone number.</p></li>
 			<li><p><strong>Footer:</strong> Copyright information. (Future: social media links.)</p></li>
