52770f64701d3ceaaaed871009112e84a66c2528
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-14 14:16:53 -0700

    centered navbar content and toggle button, added link titles

--- public_html/index.php
+++ public_html/index.php
@@ -21,6 +21,27 @@
 	<body>
 		<!-- Header with Navbar, full-width image, title -->
 		<header>
+			<div class="container">
+				<nav class="navbar navbar-expand-sm navbar-light justify-content-center">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav ml-auto mr-auto">
+							<li class="nav-item">
+								<a class="nav-link" href="#">About</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Services</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
 
 
 		</header>
