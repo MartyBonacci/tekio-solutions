ea5817a3688fabf2e036e6dcea8bd856e95c5504
Jolynn Pruitt <jo@tekiosolutions.com>
2018-02-17 12:26:26 -0700

    replaced responsive image with jumbotron, still below navbar

--- public_html/index.php
+++ public_html/index.php
@@ -8,7 +8,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 		<!-- Custom CSS -->
-<!--		<link rel="stylesheet" href="css/style.css" type="text/css"> -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
 
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<!-- Used for PWP ... optional otherwise -->
@@ -41,7 +41,13 @@
 						</div>
 					</nav>
 				</div>
-				<img src="images/office-1.jpg" class="img-fluid" alt="Responsive image">
+				<div class="jumbotron jumbotron-fluid">
+					<div class="container align-middle">
+						<h1 class="display-4 font-weight-bold text-white text-center">Tekio Solutions, LLC</h1>
+						<p class="lead font-italic text-white text-center">Adaptable Answers for Your Business</p>
+					</div>
+				</div>
+<!--				<img src="images/office-1.jpg" class="img-fluid" alt="Responsive image"> -->
 			</header>
 
 
